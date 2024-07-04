b67511a53f497ba3355386cd3f05ba5b549052a9
“jiahongyang“ <hongyooo@icloud.com>
2024-07-04 17:11:48 +0800

    v.1.7.7

--- library/src/main/java/com/hzkj/sacn/CameraTexturePreview.java
+++ library/src/main/java/com/hzkj/sacn/CameraTexturePreview.java
@@ -10,8 +10,6 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Display;
 import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
